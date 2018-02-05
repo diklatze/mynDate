7df9c6cae366cbd7912836d78f6516ac17ff46b3
Merav Shahor <Merav.Shahor@fundtech.isr>
2018-02-05 10:22:38 +0200

    add  AuthenticationPage

--- src/pages/authentication/authentication.ts
+++ src/pages/authentication/authentication.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ListPage } from '../list/list';
-import { AuthenticationPage } from '../authentication/authentication';
-
 
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html'
+  selector: 'page-signIn',
+  templateUrl: 'signIn.html'
 })
-export class RegisterPage {
+export class SignInPage {
 
   constructor(public navCtrl: NavController) {
 
