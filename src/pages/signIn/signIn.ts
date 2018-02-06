2e64b2d89c5c4fcc97d1c5f4a90bb1fd1dda8125
Merav Shahor <Merav.Shahor@fundtech.isr>
2018-02-06 11:15:41 +0200

    add

--- src/pages/signIn/signIn.ts
+++ src/pages/signIn/signIn.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ListPage } from '../list/list';
+import {AddAccountPage} from '../addAccount/addAcount';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-signIn',
@@ -8,10 +10,17 @@ import { ListPage } from '../list/list';
 })
 export class SignInPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
   }
 
+  goToAddAccount(){
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+    this.navCtrl.push(AddAccountPage);
+  }
  
 
 }
