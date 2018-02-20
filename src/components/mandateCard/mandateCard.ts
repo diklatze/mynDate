f7e0c630d04f07b14be4f6911293daa6c5ee8f30
Merav Shahor <Merav.Shahor@fundtech.isr>
2018-02-20 16:42:23 +0200

    connect mandate card to view and add mandate pages

--- src/components/mandateCard/mandateCard.ts
+++ src/components/mandateCard/mandateCard.ts
@@ -1,6 +1,10 @@
+
 import { Component,Input , Output} from '@angular/core';
 import { UserDetails } from '../../classes/UserDetails';
-
+import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
+import {AddMandatePage} from   '../../pages/addMandate/addMandate';
+import {viewMandateDetailesPage} from '../../pages//viewMandateDetailes/viewMandateDetailes'
 
 
 
@@ -12,10 +16,17 @@ export class MandateCard {
 
     @Input() account:Account;
 
-  constructor() {
+
+    constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+    }
+
+  goToAddMandate(){
+    this.navCtrl.push(AddMandatePage);
   }
 
-
+  viewMandate(){
+    this.navCtrl.push(viewMandateDetailesPage);
+  }
  
 
  
