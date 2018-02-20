5aae5419d7d33a0784b3b1976145bd60eec45a31
Merav Shahor <Merav.Shahor@fundtech.isr>
2018-02-20 09:38:23 +0200

    add user id also to user detailes

--- src/classes/UserDetails.ts
+++ src/classes/UserDetails.ts
@@ -2,6 +2,7 @@ import { Account } from './Account';
 
 export class UserDetails {
     name: string; 
+    userId : String;
     email: string;
     password: string;
     phoneNumber: string;
--- src/classes/mock-users.ts
+++ src/classes/mock-users.ts
@@ -6,20 +6,19 @@ import { Mandate } from './Mandate';
 export const restrictions: Restrictions = { notifyMoreThanUsual: true, notifyOnPayment: true };
 
 export const MANDETES: Mandate[] = [
-    { CustomerId: "32545243445", userId: "John.lennon@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", 
-    restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
-    { CustomerId: "32545243445", userId: "merav.shahor@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
-    { CustomerId: "1234555555", userId: "dikla.weil@gmail.com", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions,imagePath:".\assets\images\companyLogos\virgin_media-bundle_internet_phone_tv.png" }, 
-    { CustomerId: "6621243445", userId: "merav.shahor@gmail.com", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions ,imagePath:".\assets\images\companyLogos\british_gas.png" }, 
-    { CustomerId: "5487989898", userId: "merav.shahor@gmail.com", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions ,imagePath:".\assets\images\companyLogos\O2_cellphone.png"}, 
-    { CustomerId: "32545243445", userId: "dikla.weil@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png"}, 
-    { CustomerId: "32545243445", userId: "boaz.shuker@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
-    { CustomerId: "32545243445", userId: "shani.keshet@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
-    { CustomerId: "32545243445", userId: "rochi.benMor@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
-    { CustomerId: "32545243775", userId: "boaz.shuker@gmail.com", serviceProviderName: "Direct Énergie", serviceProviderArea: "Electric company", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Direct Énergie.png" }, 
-    { CustomerId: "6621243445", userId: "shani.keshet@gmail.com", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions,imagePath:".\assets\images\companyLogos\british_gas.png" },
-    { CustomerId: "5487989898", userId: "John.lennon@gmail.com", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions,imagePath:".\assets\images\companyLogos\O2_cellphone.png"},
-    {CustomerId: "1234555555", userId: "John.lennon@gmail.com", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions,imagePath:".\assets\images\companyLogos\virgin_media-bundle_internet_phone_tv.png" }
+    { CustomerId: "32545243445", userId: "123456789", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
+    { CustomerId: "32545243445", userId: "046259735", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
+    { CustomerId: "1234555555", userId: "208056789", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions,imagePath:".\assets\images\companyLogos\virgin_media-bundle_internet_phone_tv.png" }, 
+    { CustomerId: "6621243445", userId: "046259735", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions ,imagePath:".\assets\images\companyLogos\british_gas.png" }, 
+    { CustomerId: "5487989898", userId: "046259735", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions ,imagePath:".\assets\images\companyLogos\O2_cellphone.png"}, 
+    { CustomerId: "32545243445", userId: "208056789", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png"}, 
+    { CustomerId: "32545243445", userId: "30897459", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
+    { CustomerId: "32545243445", userId: "126987745", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
+    { CustomerId: "32545243445", userId: "3069724498", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Allianze-insurance.png" }, 
+    { CustomerId: "32545243775", userId: "30897459", serviceProviderName: "Direct Énergie", serviceProviderArea: "Electric company", restrictions: restrictions,imagePath:".\assets\images\companyLogos\Direct Énergie.png" }, 
+    { CustomerId: "6621243445", userId: "126987745", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions,imagePath:".\assets\images\companyLogos\british_gas.png" },
+    { CustomerId: "5487989898", userId: "123456789", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions,imagePath:".\assets\images\companyLogos\O2_cellphone.png"},
+    {CustomerId: "1234555555", userId: "123456789", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions,imagePath:".\assets\images\companyLogos\virgin_media-bundle_internet_phone_tv.png" }
 
 ];
 export const ACCOUNTS: Account[] = [
@@ -35,17 +34,17 @@ export const ACCOUNTS: Account[] = [
 
 
 export const USERS: UserDetails[] = [
-    { name: "John", familyName: "Lennon", email: "John.lennon@gmail.com", password: "abc123", phoneNumber: "+12326795543",accounts:ACCOUNTS },
-    { name: "Merav", familyName: "Shahor", email: "merav.shahor@gmail.com", password: "abc123", phoneNumber: "+972528543238",accounts:ACCOUNTS },
-    { name: "Dikla", familyName: "Weil", email: "dikla.weil@gmail.com", password: "abc123", phoneNumber: "+97215369715",accounts:ACCOUNTS },
-    { name: "Boaz", familyName: "Shuker", email: "boaz.shuker@gmail.com", password: "abc123", phoneNumber: "+9726351429",accounts:ACCOUNTS },
-    { name: "Shani", familyName: "Keshet", email: "shani.keshet@gmail.com", password: "abc123", phoneNumber: "+97258796452",accounts:ACCOUNTS },
-    { name: "Rochai", familyName: "Ben-Mor", email: "rochi.benMor@gmail.com", password: "abc123", phoneNumber: "+97253689742",accounts:ACCOUNTS },
-    { name: "Amir", familyName: "Shahor", email: "amir.shahor@gmail.com", password: "abc123", phoneNumber: "+972506222966",accounts:ACCOUNTS },
-    { name: "Tair", familyName: "Shahor", email: "tair.shahor@gmail.com", password: "abc123", phoneNumber: "+9999999999",accounts:ACCOUNTS },
-    { name: "Golan", familyName: "Shahor", email: "golan.shahor@gmail.com", password: "abc123", phoneNumber: "+888888888",accounts:ACCOUNTS },
-    { name: "Yarden", familyName: "Shahor", email: "yarden.shahor@gmail.com", password: "abc123", phoneNumber: "+777777777",accounts:ACCOUNTS },
-    { name: "Arbel", familyName: "Shahor", email: "arbel.shahor@gmail.com", password: "abc123", phoneNumber: "+9725645645",accounts:ACCOUNTS },
+    { name: "John", familyName: "Lennon", userId: "123456789",email: "John.lennon@gmail.com", password: "abc123", phoneNumber: "+12326795543",accounts:ACCOUNTS },
+    { name: "Merav", familyName: "Shahor",userId: "046259735", email: "merav.shahor@gmail.com", password: "abc123", phoneNumber: "+972528543238",accounts:ACCOUNTS },
+    { name: "Dikla", familyName: "Weil", userId: "208056789",email: "dikla.weil@gmail.com", password: "abc123", phoneNumber: "+97215369715",accounts:ACCOUNTS },
+    { name: "Boaz", familyName: "Shuker", userId: "30897459",email: "boaz.shuker@gmail.com", password: "abc123", phoneNumber: "+9726351429",accounts:ACCOUNTS },
+    { name: "Shani", familyName: "Keshet",userId: "126987745", email: "shani.keshet@gmail.com", password: "abc123", phoneNumber: "+97258796452",accounts:ACCOUNTS },
+    { name: "Rochai", familyName: "Ben-Mor", userId: "3069724498",email: "rochi.benMor@gmail.com", password: "abc123", phoneNumber: "+97253689742",accounts:ACCOUNTS },
+    { name: "Amir", familyName: "Shahor",userId: "040535304", email: "amir.shahor@gmail.com", password: "abc123", phoneNumber: "+972506222966",accounts:ACCOUNTS },
+    { name: "Tair", familyName: "Shahor", userId: "3026584257",email: "tair.shahor@gmail.com", password: "abc123", phoneNumber: "+9999999999",accounts:ACCOUNTS },
+    { name: "Golan", familyName: "Shahor",userId: "3025698741", email: "golan.shahor@gmail.com", password: "abc123", phoneNumber: "+888888888",accounts:ACCOUNTS },
+    { name: "Yarden", familyName: "Shahor", userId: "305967410",email: "yarden.shahor@gmail.com", password: "abc123", phoneNumber: "+777777777",accounts:ACCOUNTS },
+    { name: "Arbel", familyName: "Shahor", userId: "20153647",email: "arbel.shahor@gmail.com", password: "abc123", phoneNumber: "+9725645645",accounts:ACCOUNTS },
 
 
 
