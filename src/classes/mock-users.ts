1510280cbe4560d78bb48777a6c6ab2602667ea3
Merav Shahor <Merav.Shahor@fundtech.isr>
2018-02-20 08:59:16 +0200

    add more detailes

--- src/classes/mock-users.ts
+++ src/classes/mock-users.ts
@@ -6,19 +6,46 @@ import { Mandate } from './Mandate';
 export const restrictions: Restrictions = { notifyMoreThanUsual: true, notifyOnPayment: true };
 
 export const MANDETES: Mandate[] = [
-    { CustomerId: "32545243445", userId: "201038312", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions,imagePath:"" }, 
-    { CustomerId: "6621243445", userId: "201038312", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions,imagePath:"" },
-    { CustomerId: "121263645", userId: "201038312", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions,imagePath:""},
-    {CustomerId: "3243436645", userId: "201038312", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions,imagePath:""}
+    { CustomerId: "32545243445", userId: "John.lennon@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions }, 
+    { CustomerId: "32545243445", userId: "merav.shahor@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions }, 
+    { CustomerId: "1234555555", userId: "dikla.weil@gmail.com", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions }, 
+    { CustomerId: "6621243445", userId: "merav.shahor@gmail.com", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions }, 
+    { CustomerId: "5487989898", userId: "merav.shahor@gmail.com", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions }, 
+    { CustomerId: "32545243445", userId: "dikla.weil@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions }, 
+    { CustomerId: "32545243445", userId: "boaz.shuker@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions }, 
+    { CustomerId: "32545243445", userId: "shani.keshet@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions }, 
+    { CustomerId: "32545243445", userId: "rochi.benMor@gmail.com", serviceProviderName: "Allianz", serviceProviderArea: "Insurance", restrictions: restrictions }, 
+    { CustomerId: "32545243775", userId: "boaz.shuker@gmail.com", serviceProviderName: "Direct Énergie", serviceProviderArea: "Electric company", restrictions: restrictions }, 
+    { CustomerId: "6621243445", userId: "shani.keshet@gmail.com", serviceProviderName: "British Gas", serviceProviderArea: "Electric company", restrictions: restrictions },
+    { CustomerId: "5487989898", userId: "John.lennon@gmail.com", serviceProviderName: "O2", serviceProviderArea: "Cellphone provider", restrictions: restrictions},
+    {CustomerId: "1234555555", userId: "John.lennon@gmail.com", serviceProviderName: "Virgin Media", serviceProviderArea: "TV/Phone/Cell bundle provider", restrictions: restrictions}
 
 ];
 export const ACCOUNTS: Account[] = [
-    { bankName: "Barclays", accountNum: "20474290124893", branchName: "02B",mandates:MANDETES,imagePath:".\assets\images\bankLogos\barclays.png"}
+    { bankName: "Barclays", accountNum: "20474290124893", branchName: "02B",mandates:MANDETES },
+    { bankName: "HSBC", accountNum: "20445454124893", branchName: "151",mandates:MANDETES },
+    { bankName: "HSBC", accountNum: "2047426541893", branchName: "0CD",mandates:MANDETES },
+    { bankName: "SWED", accountNum: "20474244444893", branchName: "DEQ",mandates:MANDETES },
+    { bankName: "BOI", accountNum: "20474233333893", branchName: "NEG",mandates:MANDETES },
+    { bankName: "SWED", accountNum: "204742128744893", branchName: "B2R",mandates:MANDETES },
+    { bankName: "Barclays", accountNum: "20197224893", branchName: "02B",mandates:MANDETES }
 
 ];
 
 
 export const USERS: UserDetails[] = [
-    { name: "John", familyName: "Lennon", email: "John.lennon@gmail.com", password: "abc123", phoneNumber: "26795543",accounts:ACCOUNTS },
+    { name: "John", familyName: "Lennon", email: "John.lennon@gmail.com", password: "abc123", phoneNumber: "+12326795543",accounts:ACCOUNTS },
+    { name: "Merav", familyName: "Shahor", email: "merav.shahor@gmail.com", password: "abc123", phoneNumber: "+972528543238",accounts:ACCOUNTS },
+    { name: "Dikla", familyName: "Weil", email: "dikla.weil@gmail.com", password: "abc123", phoneNumber: "+97215369715",accounts:ACCOUNTS },
+    { name: "Boaz", familyName: "Shuker", email: "boaz.shuker@gmail.com", password: "abc123", phoneNumber: "+9726351429",accounts:ACCOUNTS },
+    { name: "Shani", familyName: "Keshet", email: "shani.keshet@gmail.com", password: "abc123", phoneNumber: "+97258796452",accounts:ACCOUNTS },
+    { name: "Rochai", familyName: "Ben-Mor", email: "rochi.benMor@gmail.com", password: "abc123", phoneNumber: "+97253689742",accounts:ACCOUNTS },
+    { name: "Amir", familyName: "Shahor", email: "amir.shahor@gmail.com", password: "abc123", phoneNumber: "+972506222966",accounts:ACCOUNTS },
+    { name: "Tair", familyName: "Shahor", email: "tair.shahor@gmail.com", password: "abc123", phoneNumber: "+9999999999",accounts:ACCOUNTS },
+    { name: "Golan", familyName: "Shahor", email: "golan.shahor@gmail.com", password: "abc123", phoneNumber: "+888888888",accounts:ACCOUNTS },
+    { name: "Yarden", familyName: "Shahor", email: "yarden.shahor@gmail.com", password: "abc123", phoneNumber: "+777777777",accounts:ACCOUNTS },
+    { name: "Arbel", familyName: "Shahor", email: "arbel.shahor@gmail.com", password: "abc123", phoneNumber: "+9725645645",accounts:ACCOUNTS },
+
+
 
 ];
